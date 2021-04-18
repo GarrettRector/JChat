b3f04a387c2f72cd316c3df4f745d509922f2d63
Garrett Rector <garrettrector329@gmail.com>
2021-04-17 22:38:18 -0400

    Create login.java

---
+++ clientFlies/login.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class login {
+    public static void main(String[] a) {
+        UI frame = new UI();
+        frame.setTitle("Login Form");
+        frame.setVisible(true);
+        frame.setBounds(10, 10, 370, 600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+
+    }
+}
