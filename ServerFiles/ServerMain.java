e56052d45b71d52a38b321970f5d11b7112dc8b1
Garrett Rector <garrettrector329@gmail.com>
2021-04-21 15:04:00 -0400

    Update ServerMain.java

--- ServerFiles/ServerMain.java
+++ ServerFiles/ServerMain.java
@@ -1,9 +1,24 @@
 package com.muc;
 
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class ServerMain {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         int port = 8818;
-        Server server =  new Server(port);
+        Server server = new Server(port);
         server.start();
+        ServerSocket Server = new ServerSocket(8505);
+        while(true)
+        {
+            Socket client = Server.accept();
+            InputStream in = client.getInputStream();
+            InputStreamReader inr = new InputStreamReader(in);
+            BufferedReader br = new BufferedReader(inr);
+            String str = br.readLine();
+            PrintStream ps = new PrintStream(client.getOutputStream());
+            ps.println(str);
+        }
     }
 }
