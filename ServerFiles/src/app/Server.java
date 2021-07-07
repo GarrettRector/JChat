a1824ad8a55f5c20051203597d94045a8a5b28c8
GarrettRector <garrett@grector.dev>
2021-07-07 10:39:52 -0400

    Prepared structure for API

---
+++ ServerFiles/src/app/Server.java
@@ -0,0 +1,50 @@
+package app;
+
+import org.apache.commons.lang3.ArrayUtils;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Server extends Thread {
+    public static String[] IPs = {};
+    private final int serverPort;
+    private final ArrayList<ServerWorker> workerList = new ArrayList<>();
+
+    public Server(int serverPort) {
+        this.serverPort = serverPort;
+    }
+
+    public List<ServerWorker> getWorkerList() {
+        return workerList;
+    }
+
+    @Override
+    public void run() {
+        try {
+            ServerSocket serverSocket = new ServerSocket(serverPort);
+            DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH:mm:ss");
+            LocalDateTime now = LocalDateTime.now();
+            System.out.println("Server Online at " + dtf.format(now));
+            while(true) {
+                Socket clientSocket = serverSocket.accept();
+                String IP = clientSocket.getRemoteSocketAddress().toString();
+                IPs = ArrayUtils.add(IPs, IP);
+                System.out.println("Accepted connection from " + clientSocket);
+                ServerWorker worker = new ServerWorker(this, clientSocket);
+                workerList.add(worker);
+                worker.start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void removeWorker(ServerWorker serverWorker) {
+        workerList.remove(serverWorker);
+    }
+}
---
+++ ServerFiles/src/app/ServerLogger.java
@@ -0,0 +1,44 @@
+package app;
+
+import java.io.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Random;
+import java.applet.*;
+
+public class ServerLogger extends Applet{
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader reader = new BufferedReader(
+                new InputStreamReader(System.in));
+        String login = reader.readLine();
+        String password = reader.readLine();
+        String bot = reader.readLine();
+
+        Path loginPath = Paths.get("ServerFiles/src/login.csv");
+        FileWriter fw = new FileWriter(String.valueOf(loginPath),true);
+        BufferedWriter bw = new BufferedWriter(fw);
+        bw.write("\n");
+        bw.write(login);
+        bw.write(",");
+        bw.write(password);
+        bw.write(",");
+        bw.write(getRandStr());
+        bw.write(",");
+        bw.write("no");
+        bw.write(",");
+        bw.write("null");
+        bw.close();
+    }
+
+    public static String getRandStr() {
+        String CHARS = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz123456789";
+        StringBuilder string = new StringBuilder();
+        Random rnd = new Random();
+        while (string.length() < 7) {
+            int index = (int) (rnd.nextFloat() * CHARS.length());
+            string.append(CHARS.charAt(index));
+        }
+        return string.toString();
+    }
+}
